2e29f20b1917bcd7ed0aeb06e86c2a1ec22ea6ad
ziaochina <ziaochina@gmail.com>
2017-06-22 14:36:19 +0800

    zlj

--- assets/app/demo/webpack.config.js
+++ assets/app/demo/webpack.config.js
@@ -4,18 +4,34 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var plugins = [
     new webpack.DefinePlugin({
-        "process.env.NODE_ENV": JSON.stringify('production')
-    }),
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
     new HtmlWebpackPlugin({
         filename: './index.html', //生成的html存放路径，相对于 path
         template: './index.html', //html模板路径
         inject: true, //允许插件修改哪些内容，包括head与body`
     }),
-];
+]
+
+if (process.env.COMPRESS) {
+    plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compressor: {
+                warnings: false
+            }
+        })
+    );
+}
+
+plugins.push(new webpack.optimize.CommonsChunkPlugin('vendor'))
 
 module.exports = {
     devtool: 'source-map',
-    entry: ["./index.js"],
+
+    entry: {
+        bundle: ["./index.js"],
+        vendor: ["react", 'react-dom', 'xr-app-loader']
+    }
 
     output: {
         path: path.join(__dirname, "/dist/"),
