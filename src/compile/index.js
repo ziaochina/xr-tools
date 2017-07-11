bf4935380f23eea490832f87746b22a2d12dea90
ziaochina <ziaochina@gmail.com>
2017-07-11 19:13:24 +0800

    zlj

--- src/compile/index.js
+++ src/compile/index.js
@@ -49,8 +49,10 @@ function buildIndex(appFolder) {
 	findApps(basePath)
 	console.log(apps)
 
+	apps.map(o=>console.log(o.path))
+	console.log(1111111)	
 	apps.map(o=>console.log(path.relative(o.path)))
-console.log(1111111)	
+console.log(22222)	
 	/*
 	import _src from '../index.app'
 	import _src_apps_about from '../apps/about/index.app'
